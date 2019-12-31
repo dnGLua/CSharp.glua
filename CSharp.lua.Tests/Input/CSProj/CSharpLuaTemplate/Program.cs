32e7e52378514f2ef5a220b26146edb229ec592f
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-12-31 14:48:18 +0100

    Add API and Test projects.

---
+++ CSharp.lua.Tests/Input/CSProj/CSharpLuaTemplate/Program.cs
@@ -0,0 +1,13 @@
+using static War3Api.Common;
+
+namespace War3Map.FZero.Source
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            FogEnable(false);
+            FogMaskEnable(false);
+        }
+    }
+}
