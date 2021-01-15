a39b997c85c5edc97ff83b387a50bd57d07841c5
Cheatoid <Cheatoid@users.noreply.github.com>
2021-01-15 01:42:45 +0100

    Move FileCoreSystemProvider to own file

--- CSharp.glua.Compiler/EmbeddedCoreSystemProvider.cs
+++ CSharp.glua.Compiler/EmbeddedCoreSystemProvider.cs
@@ -38,22 +38,4 @@ namespace CSharp.glua {
       return (name, CoreSystemFiles[name]);
     }
   }
-
-  public sealed class FileCoreSystemProvider : ICoreSystemProvider {
-    public FileCoreSystemProvider(string includeFolder) {
-      IncludeFolder = includeFolder.Length == 0
-        ? AppContext.BaseDirectory
-        : Environment.ExpandEnvironmentVariables(includeFolder);
-    }
-
-    public string IncludeFolder { get; }
-
-    bool ICoreSystemProvider.Initialize()
-      => Directory.Exists(IncludeFolder);
-
-    (string, string) ICoreSystemProvider.Read(params string[] path) {
-      var name = Path.Combine(path) + ".lua";
-      return (name, File.ReadAllText(Path.Combine(IncludeFolder, name)));
-    }
-  }
 }
---
+++ CSharp.glua.Compiler/FileCoreSystemProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using CSharp.glua.CoreSystem;
+
+namespace CSharp.glua {
+  public sealed class FileCoreSystemProvider : ICoreSystemProvider {
+    public FileCoreSystemProvider(string includeFolder) {
+      IncludeFolder = includeFolder.Length == 0
+        ? AppContext.BaseDirectory
+        : Environment.ExpandEnvironmentVariables(includeFolder);
+    }
+
+    public string IncludeFolder { get; }
+
+    bool ICoreSystemProvider.Initialize()
+      => Directory.Exists(IncludeFolder);
+
+    (string, string) ICoreSystemProvider.Read(params string[] path) {
+      var name = Path.Combine(path) + ".lua";
+      return (name, File.ReadAllText(Path.Combine(IncludeFolder, name)));
+    }
+  }
+}
